f5b53e2a5b250538b28a4bcea775fc56581bf860
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-07-20 15:31:35 +0300

    Set proxy for all /api.. requests

--- frontend-vuejs/config/index.js
+++ frontend-vuejs/config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // proxy all webpack dev-server requests starting with /api to our Spring Boot backend (localhost:8088)
+      '/api': {
+        target: 'http://localhost:8088',
+        changeOrigin: true
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
