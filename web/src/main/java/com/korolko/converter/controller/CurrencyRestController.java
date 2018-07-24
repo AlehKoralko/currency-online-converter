1859ba7db1b0f5ed1bc1c6d3c2f2915d0dc43e9a
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-07-25 00:48:08 +0300

    Added 2 methods

--- web/src/main/java/com/korolko/converter/controller/CurrencyRestController.java
+++ web/src/main/java/com/korolko/converter/controller/CurrencyRestController.java
@@ -2,21 +2,24 @@ package com.korolko.converter.controller;
 
 import com.korolko.converter.entity.Currency;
 import com.korolko.converter.service.CurrencyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
 public class CurrencyRestController {
 
     private final CurrencyService currencyService;
+
+    private final Logger logger = LoggerFactory.getLogger(CurrencyRestController.class);
 
     @Autowired
     public CurrencyRestController(CurrencyService currencyService) {
@@ -28,9 +31,35 @@ public class CurrencyRestController {
         List<Currency> currencies = currencyService.getAll();
 
         if (currencies == null) {
+            logger.info("Currencies are not found.");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
+        logger.info("Get all currencies.");
         return new ResponseEntity<>(currencyService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Currency> getCurrencyById(@PathVariable("id") long id) {
+        Optional<Currency> currency = currencyService.getById(id);
+
+        if (! currency.isPresent()) {
+            logger.info("Currency with id - " + id + " not found.");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        logger.info("Get currency with id - " + id);
+        return new ResponseEntity<>(currency.get(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/convert", params = {"current", "target", "amount"},
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Double> convert(@RequestParam("current") String currentAbbr,
+                                        @RequestParam("target") String targetAbbr,
+                                        @RequestParam("amount") int amount) {
+
+        double convertValue = currencyService.convert(currentAbbr, targetAbbr, amount);
+
+        return new ResponseEntity<>(convertValue, HttpStatus.OK);
     }
 }
