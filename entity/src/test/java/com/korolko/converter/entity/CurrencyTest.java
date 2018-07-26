6617daa07e8420b2e33649d2df1d6f3f9b9be6a9
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-07-26 16:28:23 +0300

    Change test

--- entity/src/test/java/com/korolko/converter/entity/CurrencyTest.java
+++ entity/src/test/java/com/korolko/converter/entity/CurrencyTest.java
@@ -10,8 +10,8 @@ public class CurrencyTest {
     @Test
     public void convertTo() {
         final Currency current = new Currency(1L, "Беллоруский рубль", "BYN", 1d, 1);
-        final Currency target = new Currency(2L, "Доллар США", "USD", 2d, 1);
+        final Currency target = new Currency(2L, "Доллар США", "USD", 1.9901d, 1);
 
-        Assert.assertEquals(5, current.convertTo(target, 10), 0);
+        Assert.assertEquals("2.01", current.convertTo(target, 4).toString());
     }
 }
