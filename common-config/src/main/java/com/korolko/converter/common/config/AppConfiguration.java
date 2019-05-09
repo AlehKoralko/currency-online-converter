8856770f30418902375d7727f2558dc0e4dc5d32
Aleh Karalko <aleh_karalko@epam.com>
2019-05-09 22:58:34 +0300

    Enable scheduler

--- common-config/src/main/java/com/korolko/converter/common/config/AppConfiguration.java
+++ common-config/src/main/java/com/korolko/converter/common/config/AppConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
+@EnableScheduling
 public class AppConfiguration {
 
     @Value("${currency.api.url}")
